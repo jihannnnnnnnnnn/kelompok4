f1b447deb2c22193ff86012977ccc7e1c7248ff8
jihannnnnnnnnnn <jihanfiolalhitagresiya21@gmail.com>
2025-05-21 15:28:22 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,22 +1,64 @@
-const wrapper = document.querySelector('.wrapper');
-const loginLink = document.querySelector('.login-link');
-const registerLink = document.querySelector('.register-link');
-const btnPopup = document.querySelector('.btnLogin-popup');
-const iconClose = document.querySelector('.icon-close');
+// toggle icon navbar 
 
-registerLink.addEventListener('click', () => {
-    wrapper.classList.add('active');
-})
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('navbar');
 
-loginLink.addEventListener('click', () => {
-    wrapper.classList.remove('active');
-})
+menuIcon.onclick = ()=>{
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
 
-btnPopup.addEventListener('click', () => {
-    wrapper.classList.add('active-popup');
-})
+// scroll section active 
+let section = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('hader nav a');
 
-iconClose.addEventListener('click', () => {
-    wrapper.classList.remove('active-popup');
-})
+window.onscroll= () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+        if(top >= offset && top < offset + height){
+                navLinks.forEach(links => {
+                    links.classList.remove('active');
+                    document.querySelector('header nav a[href*='+ id + ']').classList.add('active');
+                });
+        };
 
+        
+    });
+
+    // scticky navbar 
+
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    // remove toggle icon dan navbar when click link 
+    menuIcon.classList.remove('bx-x') ;
+    navbar.classList.remove('active');
+};
+
+// scroll reveal 
+ScrollReveal({ 
+    
+    // reset: true,
+    distance: '80px', 
+    duration:2000,
+    delay:200
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .services-container, .portofolio-box, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img ', { origin: 'left' });
+ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
+
+// typed js 
+
+const typed= new typed('.multiple-text', {
+    strings : ['Frontend Developer', 'YouTuber', 'Blogger' ],
+    typeSpeed: 100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+
+});
